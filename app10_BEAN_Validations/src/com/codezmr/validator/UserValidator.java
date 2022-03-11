4b72673131786d98d67c95bc4d684df5ea0f4ff3
mdzam <mdzam@ZMR>
2022-03-11 23:18:34 +0530

    getting validation messages from Error_Messages properties file

--- app10_BEAN_Validations/src/com/codezmr/validator/UserValidator.java
+++ app10_BEAN_Validations/src/com/codezmr/validator/UserValidator.java
@@ -1,11 +1,26 @@
 package com.codezmr.validator;
 
+import java.io.IOException;
+import java.util.Properties;
+
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PropertiesLoaderUtils;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 
 import com.codezmr.beans.User;
 
 public class UserValidator implements Validator {
+
+	private Resource resource;
+	
+	public Resource getResource() {
+		return resource;
+	}
+
+	public void setResource(Resource resource) {
+		this.resource = resource;
+	}
 
 	@Override
 	public boolean supports(Class<?> cls) {
@@ -15,32 +30,42 @@ public class UserValidator implements Validator {
 
 	@Override
 	public void validate(Object obj, Errors errors) {
-		User user = (User) obj;
-		if(user.getUname() == null || user.getUname().equals("")){
+		try {
+		    
+			User user = (User) obj;
 			
-			errors.rejectValue("uname", "error.uname.required", "User Name Is Required!!");
-		  }
-	       
-		if(user.getUpwd() == null || user.getUpwd().equals("")){
-			
-			errors.rejectValue("upwd", "error.upwd.required", "User Password Is Required!!");
-		  }
-	       
-		if(user.getUage() == 0){
-			
-			errors.rejectValue("uage", "error.uage.required", "User Age Is Required!!");
-		  }
+			Properties prop = PropertiesLoaderUtils.loadProperties(resource);
 		
-		if(user.getUemail() == null || user.getUemail().equals("")){
 			
-			errors.rejectValue("uemail", "error.uemail.required", "User Email Is Required!!");
-		  }
-		
-		if(user.getUmobile() == null || user.getUmobile().equals("")){
+			if(user.getUname() == null || user.getUname().equals("")){
+				
+				errors.rejectValue("uname", "error.uname.required", prop.getProperty("error.uname.required"));
+			  }
+		       
+			if(user.getUpwd() == null || user.getUpwd().equals("")){
+				
+				errors.rejectValue("upwd", "error.upwd.required", prop.getProperty("error.upwd.required"));
+			  }
+		       
+			if(user.getUage() == 0){
+				
+				errors.rejectValue("uage", "error.uage.required", prop.getProperty("error.uage.required"));
+			  }
 			
-			errors.rejectValue("umobile", "error.umobile.required", "User Mobile Is Required!!");
-		  }
+			if(user.getUemail() == null || user.getUemail().equals("")){
+				
+				errors.rejectValue("uemail", "error.uemail.required", prop.getProperty("error.uemail.required"));
+			  }
+			
+			if(user.getUmobile() == null || user.getUmobile().equals("")){
+				
+				errors.rejectValue("umobile", "error.umobile.required", prop.getProperty("error.umobile.required"));
+			  }
 
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+		}
 	}
 
 }

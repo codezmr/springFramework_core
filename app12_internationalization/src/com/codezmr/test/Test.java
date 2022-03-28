52a2fa354b8c6a9adc1e0a829732a331430a2b4e
mdzam <mdzam@ZMR>
2022-03-28 08:21:52 +0530

    Date Formation To Local

--- app12_internationalization/src/com/codezmr/test/Test.java
+++ app12_internationalization/src/com/codezmr/test/Test.java
@@ -1,5 +1,7 @@
 package com.codezmr.test;
 
+import java.util.Date;
+import java.text.DateFormat;
 import java.text.NumberFormat;
 import java.util.Locale;
 
@@ -8,8 +10,12 @@ public class Test {
 	public static void main(String[] args) {
 		
 		Locale locale = new Locale("en", "US");
+		
 		NumberFormat numberFormat = NumberFormat.getInstance(locale);
 		System.out.println(numberFormat.format(1234567890.23456788));
+		
+		DateFormat dateFormat = DateFormat.getDateInstance(0, locale);
+		System.out.println(dateFormat.format(new Date()));
 	}
 
 }
